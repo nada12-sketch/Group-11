40e6cf1e1e592c52409af34262d733b7c765bc5b
nada12-sketch <85442323+nada12-sketch@users.noreply.github.com>
2021-06-06 14:49:29 +0200

    Add files via upload

---
+++ WEEK 8 /JS/main.js
@@ -0,0 +1,11 @@
+var getusername= prompt("HELLO WORLD");
+
+console.log(getusername); 
+
+document.getElementById("username").innerText = getUserName;
+
+// tooltip example 
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
